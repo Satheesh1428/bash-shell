376a80727be5129a70484b0e06f1fe5089dd179a
ASatheesh <satheeshovsn@gmail.com>
2024-08-30 22:35:00 +0530

    nodehealth commit1

---
+++ nodehealth.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+###########################
+# Author: Satheesh
+# Date written: 30-08-2024
+# This scripts outputs node health
+# Version V1
+############################
+
+df -h
+
+free -g
+
+nproc
+
+
