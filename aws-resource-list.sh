33a320f81f31c5a81fd3b45fbfc52d4c986e4eb6
ASatheesh <satheeshovsn@gmail.com>
2024-09-01 00:54:04 +0530

    aws-resource-commit1

--- aws-resource-list.sh
+++ aws-resource-list.sh
@@ -20,6 +20,10 @@ then
 fi
 
 # check AWS CLI installed or not
+# Assign the arguments to variables and convert the service to lowercase
+aws_region=$1
+aws_service=$2
+
 if ! command -v aws &> /dev/null; then
     echo "AWS CLI is not installed. Please install the AWS CLI and try again."
     exit 1
@@ -32,3 +36,17 @@ then
    echo "AWS CLI is not configured. Please configure the AWS CLI and try again."
        exit 1
 fi
+
+# List the resources based on the service
+
+case $aws_service in
+
+    ec2)
+        echo "Listing EC2 Instances in $aws_region"
+        aws ec2 describe-instances --region $aws_region
+        ;;
+     *)
+            echo "Invalid service. Please enter a valid service."
+            exit 1
+            ;;
+esac
