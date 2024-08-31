08693c712ded8d2804ab344481ef14f10b5b841c
ASatheesh <satheeshovsn@gmail.com>
2024-08-31 11:43:25 +0530

    nodehealth commit1

--- nodehealth.sh
+++ nodehealth.sh
@@ -5,11 +5,13 @@
 # This scripts outputs node health
 # Version V1
 ############################
-set -x # debug mode
+set -x   # debug mode
+set  - e  # exit the script when there is an error
+set  -o  pipe fail   #  exit the script when pipe fail issues
+
 df -h
-
 free -g
-
 nproc
 
+ps -ef | grep /usr/bin | awk -F " " '{print $2}'
 
